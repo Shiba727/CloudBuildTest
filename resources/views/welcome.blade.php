7e18357ab1dede255b36a2a6b9d64a9d248a4475
Janice <janice@Macbookde-MacBook-Air.local>
2018-12-11 16:49:51 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello</div>
+                <div class="title">Hello Word</div>
             </div>
         </div>
     </body>
